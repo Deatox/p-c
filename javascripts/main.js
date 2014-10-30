402cafaff3166261b894f9f0f9db0cd681303f17
Deatox <ahmadelrakhawi2000@gmail.com>
2014-10-30 10:38:26 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,13 @@
+var save = {
+  protons = 0,
+  proton_add = 0;}
 
+
+function MProton(val) {
+    protons = protons + val;
+    if (protons == 1) {
+        document.getElementById("protoncount").innerHTML = "1 Proton";
+    }
+    else {
+        document.getElementById("protoncount").innerHTML = carrots + " Protons";
+}
