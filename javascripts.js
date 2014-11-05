a816ba2b131f0fbfe54f6c88dd131b6f72cf421d
Deatox <ahmadelrakhawi2000@gmail.com>
2014-11-05 11:10:20 -0600

    Update javascripts.js

--- javascripts.js
+++ javascripts.js
@@ -15,21 +15,21 @@ function Proton(val) {
 }
 
 function Neutron(val) {
-    save.protons = save.protons + val;
-    if (save.protons == 1) {
-        document.getElementById("protoncount").innerHTML = "1 Proton";
+    save.protons = save.neutrons + val;
+    if (save.neutrons == 1) {
+        document.getElementById("neutroncount").innerHTML = "1 neutron";
     }
     else {
-        document.getElementById("protoncount").innerHTML = save.protons + " protons";
+        document.getElementById("neutroncount").innerHTML = save.neutron + " neutron";
 }
 }
 
-function Proton(val) {
-    save.protons = save.protons + val;
-    if (save.protons == 1) {
-        document.getElementById("protoncount").innerHTML = "1 Proton";
+function Electron(val) {
+    save.electrons = save.electrons + val;
+    if (save.electrons == 1) {
+        document.getElementById("electroncount").innerHTML = "1 Electron";
     }
     else {
-        document.getElementById("protoncount").innerHTML = save.protons + " protons";
+        document.getElementById("electroncount").innerHTML = save.electrons + " Electrons";
 }
 }
