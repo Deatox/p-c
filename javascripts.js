eefa9fa19eab14ff06c19026b4c77412251c1848
Deatox <ahmadelrakhawi2000@gmail.com>
2014-11-05 09:21:33 -0600

    Update javascripts.js

--- javascripts.js
+++ javascripts.js
@@ -1,11 +1,13 @@
-var protons = 0;
+var save = {
+    protons: 0;
+}
 
 function Proton(val) {
-    protons = protons + val;
-    if (protons == 1) {
+    save.protons = save.protons + val;
+    if (save.protons == 1) {
         document.getElementById("protoncount").innerHTML = "1 Proton";
     }
     else {
-        document.getElementById("protoncount").innerHTML = protons + " Protons";
+        document.getElementById("protoncount").innerHTML = save.protons + " save.protons";
 }
 }
