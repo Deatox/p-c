df6cdc4be8fd9c60877e10e34c5d18f4d7781074
Deatox <ahmadelrakhawi2000@gmail.com>
2014-11-05 09:28:56 -0600

    Update javascripts.js

--- javascripts.js
+++ javascripts.js
@@ -1,5 +1,27 @@
 var save = {
     protons:0
+    neutrons:0
+    electrons:0
+}
+
+function Proton(val) {
+    save.protons = save.protons + val;
+    if (save.protons == 1) {
+        document.getElementById("protoncount").innerHTML = "1 Proton";
+    }
+    else {
+        document.getElementById("protoncount").innerHTML = save.protons + " protons";
+}
+}
+
+function Neutron(val) {
+    save.protons = save.protons + val;
+    if (save.protons == 1) {
+        document.getElementById("protoncount").innerHTML = "1 Proton";
+    }
+    else {
+        document.getElementById("protoncount").innerHTML = save.protons + " protons";
+}
 }
 
 function Proton(val) {
